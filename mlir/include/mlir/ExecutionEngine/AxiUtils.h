2f63440b98f02b30d9151c8a887cdca7a144785c
Nicolas Agostini <n.b.agostini@gmail.com>
2021-08-19 07:53:26 -0700

    Add AXI4MLIR files to cmake compilation

---
+++ mlir/include/mlir/ExecutionEngine/AxiUtils.h
@@ -0,0 +1,17 @@
+//===- AxUtils.h - Utils for debugging MLIR execution -----------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file declares AXI4MLIR functions to be called by the host to communicate
+// with AXI enabled accelerators.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef EXECUTIONENGINE_AXIUTILS_H_
+#define EXECUTIONENGINE_AXIUTILS_H_
+
+#endif // EXECUTIONENGINE_AXIUTILS_H_
