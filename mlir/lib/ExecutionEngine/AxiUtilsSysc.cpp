765c4d5bce7502d32da5a485cb1737249d6e8d73
Nicolas Agostini <n.b.agostini@gmail.com>
2021-09-23 04:31:02 -0700

    Apply clang-format

--- mlir/lib/ExecutionEngine/AxiUtilsSysc.cpp
+++ mlir/lib/ExecutionEngine/AxiUtilsSysc.cpp
@@ -26,78 +26,83 @@ extern "C" void dma_init(unsigned int dma_address,
                          unsigned int dma_input_buffer_size,
                          unsigned int dma_output_address,
                          unsigned int dma_output_buffer_size) {
-  std::cout << "Called: " << __func__ << "  sysc version" << std::endl;
+  std::cout << "Called: " << __func__ << " sysc version" << std::endl;
   std::cout << "\t" << dma_address << std::endl;
   std::cout << "\t" << dma_input_address << std::endl;
   std::cout << "\t" << dma_input_buffer_size << std::endl;
   std::cout << "\t" << dma_output_address << std::endl;
   std::cout << "\t" << dma_output_buffer_size << std::endl;
+
+  myDMA.dma_init(dma_address, dma_input_address, dma_input_buffer_size,
+                 dma_output_address, dma_output_buffer_size);
   return;
 }
 
 extern "C" void dma_free() {
-  std::cout << "Called: " << __func__ << "  sysc version" << std::endl;
+  std::cout << "Called: " << __func__ << " sysc version" << std::endl;
   myDMA.dma_free();
 }
 
 extern "C" unsigned int *dma_get_inbuffer() {
-  std::cout << "Called: " << __func__ << "  sysc version" << std::endl;
-  return 0;
+  std::cout << "Called: " << __func__ << " sysc version" << std::endl;
+  return myDMA.dma_get_inbuffer();
 }
 
 extern "C" unsigned int *dma_get_outbuffer() {
-  std::cout << "Called: " << __func__ << std::endl;
-  std::cout << "Called: " << __func__ << "  sysc version" << std::endl;
-  return 0;
+  std::cout << "Called: " << __func__ << " sysc version" << std::endl;
+  return myDMA.dma_get_outbuffer();
 }
 
 extern "C" int dma_copy_to_inbuffer(unsigned int *host_src_address,
                                     int data_length, int offset) {
-  std::cout << "Called: " << __func__ << "  sysc version" << std::endl;
-  return 0;
+  std::cout << "Called: " << __func__ << " sysc version" << std::endl;
+  return myDMA.dma_copy_to_inbuffer(host_src_address, data_length, offset);
 }
 
 extern "C" int dma_copy_from_outbuffer(unsigned int *host_dst_address,
                                        int data_length, int offset) {
-  std::cout << "Called: " << __func__ << "  sysc version" << std::endl;
-  return 0;
+  std::cout << "Called: " << __func__ << " sysc version" << std::endl;
+  return myDMA.dma_copy_from_outbuffer(host_dst_address, data_length, offset);
 }
 
 extern "C" int dma_start_send(int length, int offset) {
-  std::cout << "Called: " << __func__ << "  sysc version" << std::endl;
-  return 0;
+  std::cout << "Called: " << __func__ << " sysc version" << std::endl;
+  return myDMA.dma_start_send(length, offset);
 }
 
 extern "C" int dma_check_send() {
-  std::cout << "Called: " << __func__ << "  sysc version" << std::endl;
+  std::cout << "Called: " << __func__ << " sysc version" << std::endl;
   return 0;
 }
 
 extern "C" void dma_wait_send() {
-  std::cout << "Called: " << __func__ << "  sysc version" << std::endl;
+  std::cout << "Called: " << __func__ << " sysc version" << std::endl;
+  myDMA.dma_wait_send();
 }
 
 extern "C" int dma_start_recv(int length, int offset) {
-  std::cout << "Called: " << __func__ << "  sysc version" << std::endl;
-  return 0;
+  std::cout << "Called: " << __func__ << " sysc version" << std::endl;
+  return myDMA.dma_start_recv(length, offset);
 }
 
 extern "C" void dma_wait_recv() {
-  std::cout << "Called: " << __func__ << "  sysc version" << std::endl;
+  std::cout << "Called: " << __func__ << " sysc version" << std::endl;
+  myDMA.dma_wait_recv();
 }
 
 extern "C" int dma_check_recv() {
-  std::cout << "Called: " << __func__ << "  sysc version" << std::endl;
-  return 0;
+  std::cout << "Called: " << __func__ << " sysc version" << std::endl;
+  return myDMA.dma_check_recv();
 }
 
 extern "C" unsigned int dma_set(unsigned int *dma_virtual_address, int offset,
                                 unsigned int value) {
-  std::cout << "Called: " << __func__ << "  sysc version" << std::endl;
+  std::cout << "Called: " << __func__ << " sysc version" << std::endl;
+  myDMA.dma_set(dma_virtual_address, offset, value);
   return 0;
 }
 
 extern "C" unsigned int dma_get(unsigned int *dma_virtual_address, int offset) {
-  std::cout << "Called: " << __func__ << "  sysc version" << std::endl;
-  return 0;
+  std::cout << "Called: " << __func__ << " sysc version" << std::endl;
+  return myDMA.dma_get(dma_virtual_address, offset);
 }
