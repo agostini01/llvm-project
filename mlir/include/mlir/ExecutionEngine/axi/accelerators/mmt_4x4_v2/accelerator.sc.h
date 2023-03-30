2c6f364fdfe87c24ac6ede6a9c2a753544e23513
Jude Haris <j.haris.1@research.gla.ac.uk>
2023-03-30 18:21:58 +0100

    Updated AccV2 memory format and  fixed V4 warnings

--- mlir/include/mlir/ExecutionEngine/axi/accelerators/mmt_4x4_v2/accelerator.sc.h
+++ mlir/include/mlir/ExecutionEngine/axi/accelerators/mmt_4x4_v2/accelerator.sc.h
@@ -2,18 +2,7 @@
 #define ACC_H
 
 #include "dma_engine.sc.h"
-
-#ifndef __SYNTHESIS__
-#define DWAIT(x) wait(x)
-#else
-#define DWAIT(x)
-#endif
-
-#define M 4
-#define N 4
-#define K 4
-
-#define ACCNAME MM_4x4v2
+#define ACCNAME MMT_4x4v2
 
 #ifdef VERBOSE_ACC
 #define ALOG(x) std::cout << x << std::endl
@@ -50,9 +39,9 @@ struct opcode {
 SC_MODULE(ACCNAME) {
   sc_in<bool> clock;
   sc_in<bool> reset;
-  sc_int<32> inputs[M][K];
-  sc_int<32> weights[K][N];
-  sc_int<32> outputs[M][N];
+  sc_int<32> inputs[4096];
+  sc_int<32> weights[4096];
+  sc_int<32> outputs[4096];
   sc_fifo_in<DATA> din1;
   sc_fifo_out<DATA> dout1;
 
@@ -80,8 +69,6 @@ SC_MODULE(ACCNAME) {
 
   void print_profile();
 
-  int mul_int32(int, int);
-
   SC_HAS_PROCESS(ACCNAME);
 
   ACCNAME(sc_module_name name_) : sc_module(name_) {
@@ -95,21 +82,17 @@ SC_MODULE(ACCNAME) {
     reset_signal_is(reset, true);
 
     process_blocks = 0;
-    read_A_len = 0;
-    read_B_len = 0;
-    compute_C_len = 0;
-    send_C_len = 0;
+    read_A_len=0;
+    read_B_len=0;
+    compute_C_len=0;
+    send_C_len=0;
     verbose = false;
 
-    // clang-format off
-    // #pragma HLS RESOURCE variable=din1 core=AXI4Stream metadata="-bus_bundle S_AXIS_DATA1" port_map={{din1_0 TDATA} {din1_1 TLAST}}
-    // #pragma HLS RESOURCE variable=dout1 core=AXI4Stream metadata="-bus_bundle M_AXIS_DATA1" port_map={{dout1_0 TDATA} {dout1_1 TLAST}}
-    // #pragma HLS RESET variable=reset
-
-    // #pragma HLS array_partition variable=inputs complete dim=2
-    // #pragma HLS array_partition variable=weights complete dim=0
-    // #pragma HLS array_partition variable=outputs complete dim=2
-    // clang-format on
+    // #pragma HLS RESOURCE variable=din1 core=AXI4Stream metadata="-bus_bundle
+    // S_AXIS_DATA1" port_map={{din1_0 TDATA} {din1_1 TLAST}} #pragma HLS
+    // RESOURCE variable=dout1 core=AXI4Stream metadata="-bus_bundle
+    // M_AXIS_DATA1" port_map={{dout1_0 TDATA} {dout1_1 TLAST}} #pragma HLS
+    // RESET variable=reset
   }
 };
 
@@ -143,6 +126,7 @@ void ACCNAME::print_profile() {
   cout << "++++++++++++++++++++++++++++++++++++++++" << endl;
 }
 
+
 void ACCNAME::Recv() {
   wait();
   while (1) {
@@ -152,44 +136,40 @@ void ACCNAME::Recv() {
     opcode packet(din1.read().data);
 
     if (packet.read_A) {
-      for (int m = 0; m < M; m++) {
-        // #pragma HLS pipeline
-        for (int k = 0; k < K; k++) {
-          inputs[m][k] = din1.read().data;
-        }
-      }
-      if (verbose) {
-        cout << "=========================" << endl;
-        cout << "Read BLOCK A: " << read_A_len++ << endl;
-        cout << "=========================" << endl;
-        for (int m = 0; m < M; m++) {
-          for (int k = 0; k < K; k++)
-            cout << inputs[m][k] << ",";
-          cout << endl;
-        }
-        cout << "=========================" << endl;
+      for (int i = 0; i < 16; i++) {
+        inputs[i] = din1.read().data;
+        read_A_len++;
+        DWAIT();
       }
     }
 
     if (packet.read_B) {
-      for (int k = 0; k < K; k++) {
-        // #pragma HLS pipeline
-        for (int n = 0; n < N; n++) {
-          weights[k][n] = din1.read().data;
-        }
-      }
-      if (verbose) {
-        cout << "=========================" << endl;
-        cout << "Read BLOCK B: " << read_B_len++ << endl;
-        cout << "=========================" << endl;
-        for (int k = 0; k < K; k++) {
-          for (int n = 0; n < N; n++)
-            cout << weights[k][n] << ",";
-          cout << endl;
-        }
-        cout << "=========================" << endl;
+      for (int i = 0; i < 16; i++) {
+        weights[i] = din1.read().data;
+        read_B_len++;
+        DWAIT();
       }
     }
+
+    // DEBUG ONLY
+    if (verbose) {
+      cout << "=========================" << endl;
+      cout << "BLOCK: " << process_blocks++ << endl;
+      cout << "=========================" << endl;
+      for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++)
+          cout << inputs[i * 4 + j] << ",";
+        cout << endl;
+      }
+      cout << "=========================" << endl;
+      for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++)
+          cout << weights[i * 4 + j] << ",";
+        cout << endl;
+      }
+      cout << "=========================" << endl;
+    }
+    // DEBUG ONLY
 
     if (packet.compute_C) {
       wait();
@@ -204,31 +184,31 @@ void ACCNAME::Compute() {
   while (1) {
     while (!compute)
       wait();
-
-    for (int m = 0; m < M; m++) {
-      // #pragma HLS pipeline
-      for (int n = 0; n < N; n++) {
+    for (int i = 0; i < 4; i++) {
+      for (int w = 0; w < 4; w++) {
         int acc = 0;
-        for (int k = 0; k < K; k++) {
-          int x = inputs[m][k];
-          int y = weights[k][n];
-          acc += mul_int32(x, y);
+        for (int d = 0; d < 4; d++) {
+          int x = inputs[i * 4 + d];
+          int y = weights[w * 4 + d];
+          acc += x * y;
+          compute_C_len++;
         }
-        outputs[m][n] = acc;
+        outputs[i * 4 + w] = acc;
       }
     }
 
+    // DEBUG ONLY
     if (verbose) {
       cout << "=========================" << endl;
-      cout << "Compute BLOCK C: " << compute_C_len++ << endl;
-      cout << "=========================" << endl;
-      for (int m = 0; m < M; m++) {
-        for (int n = 0; n < N; n++)
-          cout << outputs[m][n] << ",";
+      for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++)
+          cout << outputs[i * 4 + j] << ",";
         cout << endl;
       }
       cout << "=========================" << endl;
     }
+    // DEBUG ONLY
+
     wait();
     compute.write(false);
     send.write(true);
@@ -236,25 +216,20 @@ void ACCNAME::Compute() {
   }
 }
 
-int ACCNAME::mul_int32(int x, int y) { return x * y; }
-
 void ACCNAME::Send() {
   wait();
   while (1) {
     while (!send)
       wait();
-
-    for (int m = 0; m < M; m++) {
-      // #pragma HLS pipeline
-      for (int n = 0; n < N; n++) {
-        DATA d;
-        d.tlast = false;
-        if (m == M - 1 && n == N - 1)
-          d.tlast = true;
-        d.data = outputs[m][n];
-        dout1.write(d);
-        DWAIT();
-      }
+    for (int i = 0; i < 16; i++) {
+      DATA d;
+      d.tlast = false;
+      if (i == 15)
+        d.tlast = true;
+      d.data = outputs[i];
+      dout1.write(d);
+      send_C_len++;
+      DWAIT();
     }
     send.write(false);
     wait();
--- mlir/include/mlir/ExecutionEngine/axi/api_v1.h
+++ mlir/include/mlir/ExecutionEngine/axi/api_v1.h
@@ -21,11 +21,11 @@
 #include "mlir/ExecutionEngine/axi/accelerators/conv_v1/accelerator.sc.h"
 #elif ACC_V5
 #include "mlir/ExecutionEngine/axi/accelerators/mm_4x4_v5/accelerator.sc.h"
-#elif  ACC_V4
+#elif ACC_V4
 #include "mlir/ExecutionEngine/axi/accelerators/mm_4x4_v4/accelerator.sc.h"
-#elif  ACC_V3
+#elif ACC_V3
 #include "mlir/ExecutionEngine/axi/accelerators/mm_4x4_v3/accelerator.sc.h"
-#elif  ACC_V2
+#elif ACC_V2
 #include "mlir/ExecutionEngine/axi/accelerators/mm_4x4_v2/accelerator.sc.h"
 #else
 #include "mlir/ExecutionEngine/axi/accelerators/mm_4x4_v1/accelerator.sc.h"
