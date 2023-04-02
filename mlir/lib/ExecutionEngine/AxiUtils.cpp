abcbff6173b8c4bca6b659b7418aca871fd43849
Nicolas Agostini <n.b.agostini@gmail.com>
2023-04-02 16:15:26 -0400

    Fix Cs problem for recv with offset different than zero

--- mlir/lib/ExecutionEngine/AxiUtils.cpp
+++ mlir/lib/ExecutionEngine/AxiUtils.cpp
@@ -95,30 +95,30 @@ extern "C" int copy_to_inbuffer_i32(int64_t rank, void *ptr, int offset) {
 }
 
 extern "C" int
-_mlir_ciface_copy_from_outbuffer_f32(UnrankedMemRefType<float> *M) {
-  mlir_dma_copy_from_outbuffer(DynamicMemRefType<float>(*M), 0, 0);
+_mlir_ciface_copy_from_outbuffer_f32(UnrankedMemRefType<float> *M, int offset) {
+  mlir_dma_copy_from_outbuffer(DynamicMemRefType<float>(*M), 0, offset);
   return 0;
 }
 
 extern "C" int copy_from_outbuffer_f32(int64_t rank, void *ptr, int offset) {
   UnrankedMemRefType<float> descriptor = {rank, ptr};
-  return _mlir_ciface_copy_from_outbuffer_f32(&descriptor);
+  return _mlir_ciface_copy_from_outbuffer_f32(&descriptor, offset);
 }
 
-extern "C" int
-_mlir_ciface_copy_from_outbuffer_i32(UnrankedMemRefType<int> *M) {
-  mlir_dma_copy_from_outbuffer(DynamicMemRefType<int>(*M), 0, 0);
+extern "C" int _mlir_ciface_copy_from_outbuffer_i32(UnrankedMemRefType<int> *M,
+                                                    int offset) {
+  mlir_dma_copy_from_outbuffer(DynamicMemRefType<int>(*M), 0, offset);
   return 0;
 }
 
 extern "C" int copy_from_outbuffer_i32(int64_t rank, void *ptr, int offset) {
   UnrankedMemRefType<int> descriptor = {rank, ptr};
-  return _mlir_ciface_copy_from_outbuffer_i32(&descriptor);
+  return _mlir_ciface_copy_from_outbuffer_i32(&descriptor, offset);
 }
 
-template <typename T> int 
-mlir_dma_copy_from_outbuffer(const DynamicMemRefType<T> &dst,
-                                            int data_length, int offset) {
+template <typename T>
+int mlir_dma_copy_from_outbuffer(const DynamicMemRefType<T> &dst,
+                                 int data_length, int offset) {
   D(std::cout << "Called: " << __func__ << " not mock version" << std::endl;);
   myDMA.mlir_dma_copy_from_outbuffer(dst.data, dst.rank, dst.rank, dst.offset,
                                      dst.sizes, dst.strides, offset);
